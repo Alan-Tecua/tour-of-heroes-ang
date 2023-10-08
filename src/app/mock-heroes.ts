76925b8ec9d7e19343b906bd60230531f90afdec
alan tecuapetla <alan.tecuapetla@gmail.com>
2023-10-08 21:54:06 +0900

    added heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Reaper' },
+  { id: 13, name: 'Reinhardt' },
+  { id: 14, name: 'Tobjorn' },
+  { id: 15, name: 'Genji' },
+  { id: 16, name: 'Hanzo' },
+  { id: 17, name: 'Link' },
+  { id: 18, name: 'D.va' },
+  { id: 19, name: 'Bjorn' },
+  { id: 20, name: 'Mei' },
+];
